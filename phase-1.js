d5b955c0d066544c63746901383e65822ed5c8bc
Justin <16kimj97@gmail.com>
2023-12-20 09:44:07 -0800

    first

--- phase-1.js
+++ phase-1.js
@@ -1,20 +1,59 @@
 function stretch() {
-  // Your code here 
+  return new Promise((resolve, reject) => {
+    let stretched = false
+    setTimeout(()=> {
+      stretched = true;
+      console.log("done stretching")
+      resolve(stretched);
+    }, 1000)
+  })
 }
 
-function runOnTreadmill() {
-  // Your code here 
+function runOnTreadmill(stretchedStatus) {
+  return new Promise((resolve, reject) => {
+    let run
+    if (stretchedStatus === true){
+      setTimeout(()=> {
+        run = true;
+        console.log("done running on treadmill")
+        resolve(run);
+      }, 500)
+    } else {
+      run = false
+      reject()
+    }
+  })
 }
 
-function liftWeights() {
-  // Your code here 
+function liftWeights(runStatus) {
+  return new Promise((resolve, reject) => {
+    let weights
+    if (runStatus === true){
+    setTimeout(()=> {
+      weights = true;
+      console.log("done lifting weights")
+      resolve(weights);
+    }, 2000)
+  } else {
+    weights = false;
+    reject();
+  }
+  })
+
 }
 
 function workout() {
-  // Your code here 
+  stretch()
+  .then((stretched) => runOnTreadmill(stretched))
+  .then((run) => liftWeights(run))
+  .then((weights) => {
+    console.log("done working out")
+  })
+
 }
 
 
+workout()
 /* ============================ TEST YOUR CODE ============================
 
 Run the file (`node phase-1.js`) and check your output against the expected
@@ -22,7 +61,6 @@ output.
 */
 
 
-workout();
   // should print out the following:
     // done stretching
     // done running on treadmill
